9b610299afcec78c8b9ddf6d39f52f0d84f68d16
Rafał Jankowski <s188825@student.pg.edu.pl>
2024-11-23 09:52:32 +0100

    created basic Flask app

--- func/send_to_mail.py
+++ func/send_to_mail.py
@@ -4,37 +4,30 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 import os
-from smtpd import SMTPServer
 from datetime import datetime
 
-SMTP_PORT = 2137
+SMTP_HOST = 'smtp.gmail.com'
+SMTP_PORT = 587
 EMAIL_ADDRESS = 'kan.red.bes@gmail.com'
-BODY = 'W zalaczniku umieszczamy szczegoly Panskiego zlecenia wiedzminskiego.\n\n' \
+EMAIL_PASSWORD = 'zaq1@WSX'
+BODY = 'W załączniku umieszczamy szczegóły Waszego zlecenia wiedźminskiego.\n\n' \
        'Z wyrazami szacunku\n' \
        'Kancelaria Redukcji Bestii'
-
-
-def start_smtp_server():
-    server = SMTPServer(('localhost', SMTP_PORT), None)
-    print(f'Starting SMTP server on port {SMTP_PORT}...')
-    try:
-        while True:
-            pass
-    except KeyboardInterrupt:
-        server.close()
 
 
 def send_email(recipient_email, xml_file_path, pdf_file_path):
     msg = MIMEMultipart()
     msg['From'] = EMAIL_ADDRESS
     msg['To'] = recipient_email
-    msg['Subject'] = 'Zlecenie wiedzminskie ' + datetime.now().strftime('%d.%m.%Y')
+    msg['Subject'] = 'Zlecenie wiedźminskie ' + datetime.now().strftime('%d.%m.%Y')
 
     msg.attach(MIMEText(BODY, 'plain'))
     attach_file(msg, xml_file_path)
     attach_file(msg, pdf_file_path)
 
-    with smtplib.SMTP('localhost', SMTP_PORT) as server:
+    with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as server:
+        server.starttls()
+        server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         server.send_message(msg)
 
     print(f'Email sent successfully to {recipient_email}.')
--- main.py
+++ main.py
@@ -1,14 +1,89 @@
 from threading import Thread
-
+import os
 from func.google_drive import upload_file
+import func.gotowy_generator_html
+import func.gotowy_generator_html.xml_to_html_updated
 import func.send_to_mail
 import func.pdf_reader
+import flask
+
+import func.xml_to_html
 
 
-if __name__ == '__main__':
-    # Start up an SMTP server for sending out the mails
-    # server_thread = Thread(target=func.send_to_mail.start_smtp_server)
-    # server_thread.start()
-    pass
-    # upload_file('example.json', 'example.json')
-    # upload_file('example.pdf', 'example.pdf')
+app = flask.Flask(__name__)
+app.secret_key = 'KanRedBes'
+form_data = []
+sections_count = 0
+
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    global form_data
+    flask.session['next_section'] = 1
+    form_data = []
+    return flask.render_template('index.html')
+
+
+# Route to handle file upload and processing
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    global form_data, sections_count
+    if 'file' not in flask.request.files:
+        return 'Nie wybrano pliku!', 400
+    file = flask.request.files['file']
+    if file.filename == '':
+        return 'Nie wybrano pliku!', 400
+
+    flask.session['next_section'] = 1
+    form_data = []
+    pdf_template_filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(pdf_template_filepath)
+
+    func.pdf_reader.read_pdf(pdf_template_filepath)
+    sections_count = func.xml_to_html.generate_htmls_from_xml('output.xml')
+
+    return flask.redirect(flask.url_for('handle_section', step=flask.session['next_section']))
+    
+
+@app.route('/form/section/<int:step>', methods=['GET', 'POST'])
+def handle_section(step):
+    global form_data, sections_count
+    if flask.request.method == 'GET':
+        if step == 2 and form_data[0]['Miejsce zamieszkania (wybrać jedno)'] == 'Wieś':
+            flask.session['next_section'] = step + 1
+            return flask.redirect(flask.url_for('handle_section', step=flask.session['next_section'])) 
+            
+        if step == 3 and form_data[0]['Miejsce zamieszkania (wybrać jedno)'] == 'Miasto':
+            flask.session['next_section'] = step + 1
+            return flask.redirect(flask.url_for('handle_section', step=flask.session['next_section'])) 
+
+        if step == 6 and form_data[3]['Czy zidentyfikowano potwora (wybrać jedno)'] == 'Nie':
+            flask.session['next_section'] = step + 1
+            return flask.redirect(flask.url_for('handle_section', step=flask.session['next_section'])) 
+        
+        if step == 7 and form_data[3]['Czy zidentyfikowano potwora (wybrać jedno)'] == 'Tak':
+            flask.session['next_section'] = step + 1
+            return flask.redirect(flask.url_for('handle_section', step=flask.session['next_section'])) 
+
+        return flask.render_template(f"section_{step}.html")
+    
+    form_data.append(flask.request.form.to_dict())
+    flask.session['next_section'] = step + 1
+    if flask.session['next_section'] > sections_count:
+        return flask.redirect(flask.url_for('summary'))
+    
+    return flask.redirect(flask.url_for('handle_section', step=flask.session['next_section'])) 
+
+
+@app.route('/summary')
+def summary():
+    global form_data
+    print(form_data)
+
+app.run()
+
+# upload_file('example.json', 'example.json')
+# upload_file('example.pdf', 'example.pdf')
