93e7a1653587b0b12729ad2b4ff1731fef8a8c7c
Marcin Chętnik <104270969+skipdudes@users.noreply.github.com>
2024-11-23 17:34:45 +0100

    Added linking CSS styles to HTML sections

--- func/xml_to_html.py
+++ func/xml_to_html.py
@@ -11,7 +11,20 @@ def generate_html_sections(xml_file):
         section_id = section.attrib.get("section_id", "")
         section_name = section.attrib.get("section_name", "")
 
-        html = [f"<div id='section_{section_id}'>", f"<h2>{section_name}</h2>", '<form method="post" enctype="multipart/form-data">']
+        # HTML stylesheet link
+        html = [
+            "<!DOCTYPE html>",
+            "<html>",
+            "<head>",
+            "<meta charset='utf-8'>",
+            "<link rel='stylesheet' type='text/css' href='../static/styles.css'>",
+            f"<title>{section_name}</title>",
+            "</head>",
+            "<body>",
+            f"<div id='section_{section_id}' class='section'>",
+            f"<h2>{section_name}</h2>",
+            '<form method="post" enctype="multipart/form-data">'
+        ]
 
         for element in section:
             if element.tag == "input_field":
@@ -21,19 +34,18 @@ def generate_html_sections(xml_file):
                 label = f"<label>{field_name}{'*' if required else ''}:</label>"
                 input_field = f"<input type='text' name='{field_name}' {'required' if required else ''}>"
 
-                html.append(label)
-                html.append(input_field)
+                html.append(f"<div class='form-group'>{label}{input_field}</div>")
 
             elif element.tag == "radio_button":
                 name = element.attrib.get("name", "")
                 required = element.attrib.get("required", "false") == "true"
 
-                html.append(f"<fieldset><legend>{name}{'*' if required else ''}:</legend>")
+                html.append(f"<fieldset class='form-group'><legend>{name}{'*' if required else ''}:</legend>")
 
                 for option in element:
                     option_name = option.attrib.get("option_name", "")
                     radio_input = f"<input type='radio' name='{name}' value='{option_name}' {'required' if required else ''}> {option_name}"
-                    html.append(radio_input)
+                    html.append(f"<div>{radio_input}</div>")
 
                 html.append("</fieldset>")
 
@@ -53,10 +65,13 @@ def generate_html_sections(xml_file):
         html.append("<button type='submit'>Wyślij</button>")
         html.append("</form>")
         html.append("</div>")
+        html.append("</body>")
+        html.append("</html>")
 
         sections.append("\n".join(html))
 
     return sections
+
 
 # Generate sections
 def generate_htmls_from_xml(xml_filename):
